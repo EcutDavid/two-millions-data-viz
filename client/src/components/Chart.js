05aa72741d626033593207ec4332fbbea460caad
David Guan <davidguandev@gmail.com>
2016-08-18 10:22:15 +0800

    Implement tooltip.

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -2,18 +2,19 @@ import d3 from 'd3'
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { formatDate } from '../helpers/dateFormat'
+
 import 'styles/chart.scss'
 
 const xScale = d3.scale.linear().domain([0, 100]).range([0, 460])
 
-const yAxisScale = d3.scale.linear().domain([0, 100]).range([360, 0])
-
 const xLeftPadding = 32
 const yTopPadding = 15
 
-const initChart = (data) => {
+const initChart = (data, startHour) => {
   const yScale = d3.scale.linear().domain([0, d3.max(data)]).range([0, 460])
   const svgArea = document.querySelector('.Chart svg')
+  const tooltip = d3.select('.Chart .tooltip')
 
   d3.select(svgArea).selectAll('rect')
     .attr({
@@ -37,26 +38,50 @@ const initChart = (data) => {
     })
     //Impletation of the :hover
     .on('mouseover', function() {
-      d3.select(this)
-      .attr('fill', '#03336B')
+      d3.select(this).attr('fill', '#03336B')
     })
     .on('mouseout', function() {
       d3.select(this)
-      .attr('fill', '#4A90E2')
+        .attr('fill', '#4A90E2')
+      tooltip
+        .transition()
+        .duration(2000)
+        .style('opacity', '0')
+    })
+    .on('mouseenter', function(d, i) {
+      d3.select('.Chart .tooltip .time')
+        .text(formatDate(startHour + i))
+      d3.select('.Chart .tooltip .number')
+        .text(d)
+      tooltip
+        .transition()
+        .style('opacity', 1)
+    })
+    .on('mousemove', () => {
+      const point = d3.mouse(svgArea)
+      tooltip.style('left', `${point[0] - 80}px`)
+      tooltip.style('top', `${12 + point[1]}px`)
     })
 }
 
 @connect(mapStateToProps)
 export default class Chart extends React.Component {
   componentWillUpdate(props) {
-    const { data } = props
-    if(data) initChart(data)
+    const { data, startHour } = props
+    if(data) initChart(data, startHour)
   }
 
   render() {
+    const { data, startHour } = this.props
+    if(data) initChart(data, startHour)
+
     return (
       <div className='Chart'>
         <svg />
+        <div className='tooltip'>
+          <p className='time'>26, July, 7AM</p>
+          <p className='number'>2452473800</p>
+        </div>
       </div>
     )
   }
@@ -65,9 +90,11 @@ export default class Chart extends React.Component {
 function mapStateToProps(state) {
   const data = state.data.get('data')
   const isLoding = state.status.get('isLoding')
+  const startHour = state.status.get('startHour')
 
   return {
     data,
-    isLoding
+    isLoding,
+    startHour
   }
 }
